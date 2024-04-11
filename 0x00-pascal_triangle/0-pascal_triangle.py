2620ca236c61304f1ec922c10dd4812a048f1317
Lebohang1821 <millboylebohang@gmail.com>
2024-04-11 04:37:12 -0700

    task-0(Pascal T)

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+!/usr/bin/python3
+'''The Mod for working with Pascal triangle
+'''
+
+
+def pascal_triangle(n):
+    '''It makes list of lists of integers repr
+    Pascal's triangle of given integer
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
