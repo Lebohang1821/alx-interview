6b64c5eee406ede8412b1079697787a89168e863
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-10 18:26:22 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-""" UTF-8 Validation """
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
     """
-    Method that determines if a given data set represents a valid
-    UTF-8 encoding.
+    It determines whether given data set represents valid UTF-8 encoding
     """
     num_bytes = 0
 
     first_mask = 1 << 7
     second_mask = 1 << 6
 
-    for i in data:
+    for byte in data:
 
         mask_byte = 1 << 7
 
         if num_bytes == 0:
 
-            while mask_byte & i:
+            while mask_byte & byte:
                 num_bytes += 1
-                mask_byte = mask_byte >> 1
+                mask_byte >>= 1
 
             if num_bytes == 0:
                 continue
@@ -29,7 +28,7 @@ def validUTF8(data):
                 return False
 
         else:
-            if not (i & first_mask and not (i & second_mask)):
+            if not (byte & first_mask and not (byte & second_mask)):
                     return False
 
         num_bytes -= 1
