e416d65e986cdf8e3d7155e13769f0a7cde578f8
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-30 16:41:37 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""Its the 2D matrix rotation module.
 """
 
+
 def rotate_2d_matrix(matrix):
-    """Rotates a 2D matrix 90 degrees clockwise in place.
+    """It rotates 2D matrix 90 degrees clockwise in place.
     """
     if not isinstance(matrix, list):
         return
@@ -11,21 +12,19 @@ def rotate_2d_matrix(matrix):
         return
     if not all(isinstance(row, list) for row in matrix):
         return
-    
-    num_rows = len(matrix)
-    num_cols = len(matrix[0])
-    
-    if not all(len(row) == num_cols for row in matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(len(row) == cols for row in matrix):
         return
 
-    c, r = 0, num_rows - 1
-    for i in range(num_cols * num_rows):
-        if i % num_rows == 0:
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
             matrix.append([])
         if r == -1:
-            r = num_rows - 1
+            r = rows - 1
             c += 1
         matrix[-1].append(matrix[r][c])
-        if c == num_cols - 1 and r >= -1:
+        if c == cols - 1 and r >= -1:
             matrix.pop(r)
         r -= 1
