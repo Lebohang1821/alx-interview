58823468af38d032a54f5abbc409cf3a910c9b5e
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-30 16:35:52 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,28 +3,27 @@
 """
 
 
-def rotate_matrix_90_degrees_clockwise(matrix_2d):
-    """Rotates a 2D matrix 90 degrees clockwise in place.
+def rotate_2d_matrix(matrix):
+    """Rotates an m by n 2D matrix in place.
     """
-    if not isinstance(matrix_2d, list):
+    if type(matrix) != list:
         return
-    if len(matrix_2d) == 0:
+    if len(matrix) <= 0:
         return
-    if not all(isinstance(row, list) for row in matrix_2d):
+    if not all(map(lambda x: type(x) == list, matrix)):
         return
-    num_rows = len(matrix_2d)
-    num_cols = len(matrix_2d[0])
-    if not all(len(row) == num_cols for row in matrix_2d):
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == num_cols, matrix)):
         return
-
-    col_index, row_index = 0, num_rows - 1
-    for cell_index in range(num_cols * num_rows):
-        if cell_index % num_rows == 0:
-            matrix_2d.append([])
-        if row_index == -1:
-            row_index = num_rows - 1
-            col_index += 1
-        matrix_2d[-1].append(matrix_2d[row_index][col_index])
-        if col_index == num_cols - 1 and row_index >= -1:
-            matrix_2d.pop(row_index)
-        row_index -= 1
+    c, r = 0, num_rows - 1
+    for i in range(num_cols * num_rows):
+        if i % num_rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = num_rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == num_cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
