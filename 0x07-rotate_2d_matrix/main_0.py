bbf48bb12eb00a982d9999c0892d97c892103a8b
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-30 16:46:11 +0200

    Update main_0.py

--- 0x07-rotate_2d_matrix/main_0.py
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -1,11 +1,13 @@
-t 0x07 - Rotate 2D Matrix
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
 """
 rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
 
 if __name__ == "__main__":
-        matrix = [[1, 2, 3],
-                      [4, 5, 6],
-                                    [7, 8, 9]]
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
 
-                                        rotate_2d_matrix(matrix)
-                                            print(matrix)
+    rotate_2d_matrix(matrix)
+    print(matrix)
