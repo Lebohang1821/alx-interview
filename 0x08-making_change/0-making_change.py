e8d296b62caf0d9e9fa0939e8ea58bb838fe9949
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-05 17:07:29 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,16 +9,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    rem = total
+    _rem = total
     coins_count = 0
     coin_idx = 0
     sorted_coins = sorted(coins, reverse=True)
     n = len(coins)
-    while rem > 0:
+    while _rem > 0:
         if coin_idx >= n:
             return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
+        if _rem - sorted_coins[coin_idx] >= 0:
+            _rem -= sorted_coins[coin_idx]
             coins_count += 1
         else:
             coin_idx += 1
