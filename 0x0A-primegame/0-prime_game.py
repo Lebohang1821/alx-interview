c673cc5ab509390f6e34f4fc928b9c2b10a4b932
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-20 16:13:34 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Prime game module.
+"""Module for Prime Game.
 """
 
 
 def isWinner(x, nums):
-    """Determines the winner of a prime game session with `x` rounds.
+    """It determines winner of prime game session over `x` rounds.
     """
     if x < 1 or not nums:
         return None
     marias_wins, bens_wins = 0, 0
-    # generate primes with a limit of the maximum number in nums
+    # it generate list of prime statuses up to maximum number in nums
     n = max(nums)
     primes = [True for _ in range(1, n + 1, 1)]
     primes[0] = False
+    # It implement Sieve of Eratosthenes to find all primes up to n
     for i, is_prime in enumerate(primes, 1):
         if i == 1 or not is_prime:
             continue
         for j in range(i + i, n + 1, i):
             primes[j - 1] = False
-    # filter the number of primes less than n in nums for each round
+    # It filter number of primes less than n in nums for each round
     for _, n in zip(range(x), nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
         bens_wins += primes_count % 2 == 0
